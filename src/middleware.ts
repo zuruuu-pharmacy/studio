18b8ec828951b0730fbaf8c3cea740ce3086a61d
Junaid Butt <junaidbutt123909@gmail.com>
2025-08-27 23:21:55 +0000

    now signin is not workiing

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,11 +5,13 @@ export function middleware(request: NextRequest) {
   const hasSession = request.cookies.has('session')
   const isAuthPage = request.nextUrl.pathname.startsWith('/login') || request.nextUrl.pathname.startsWith('/signup') || request.nextUrl.pathname.startsWith('/forgot-password');
  
+  // If the user is on an auth page and has a session, redirect to home
   if (isAuthPage && hasSession) {
     return NextResponse.redirect(new URL('/', request.url))
   }
  
-  if (!hasSession && !isAuthPage) {
+  // If the user is not on an auth page and does not have a session, redirect to login
+  if (!isAuthPage && !hasSession) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
  
